cd1a6de9453fa7c6d93438ae2242ee402572fa7f
linhx13 <mylhx288@gmail.com>
2018-01-15 20:18:02 +0800

    Add split_sentences.

--- pykit/nlp/utils.py
+++ pykit/nlp/utils.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 
+import re
 from pykit.utils import to_unicode
 
 
@@ -10,7 +11,7 @@ def str_half2full(s):
         code = ord(c)
         if code == 0x0020:
             code = 0x3000
-        elif 0x21 <= code <= 0x7e:
+        elif 0x0021 <= code <= 0x007e:
             code += 0xfee0
         return unichr(code)
     return ''.join(_conv(c) for c in to_unicode(s))
@@ -26,4 +27,17 @@ def str_full2half(s):
         return unichr(code)
     return ''.join(_conv(c) for c in to_unicode(s))
 
+
+__english_periods = u'\r|\n|\?!|!|\?|\. '
+__three_periods = u'？！”|。’”|！’”|……”'
+__two_periods = u'。”|！”|？”|；”|？！|……'
+__one_periods = u'！|？|｡|。|'
+
+__periods_pat = re.compile(u'(%s)' % '|'.join(
+    [__english_periods, __three_periods, __two_periods, __one_periods]))
+
+
+def split_sentences(s):
+    res = __periods_pat.split(to_unicode(s))[:-1]
+    return (''.join(res[i:i+2]).strip() for i in xrange(0, len(res), 2))
 
